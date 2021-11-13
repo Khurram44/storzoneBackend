4967e26d0b2dd08e61690de57d4d171cd425f5c2
Khurram44 <ks7844201@gmail.com>
2021-11-13 16:52:14 +0500

    update DB heroku

---
+++ config/env/production/database.js
@@ -0,0 +1,21 @@
+module.exports = ({ env }) => ({
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'mongoose',
+        settings: {
+          host: env('DATABASE_HOST', 'cluster0.nb36t.mongodb.net'),
+          srv: env.bool('DATABASE_SRV', true),
+          port: env.int('DATABASE_PORT', 27017),
+          database: env('DATABASE_NAME', 'Storzone'),
+          username: env('DATABASE_USERNAME', 'Admin'),
+          password: env('DATABASE_PASSWORD', 'Admin1234'),
+        },
+        options: {
+          authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+          ssl: env.bool('DATABASE_SSL', true),
+        },
+      },
+    },
+  });
+  
